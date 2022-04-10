fa47e7b07f5ec2a1f813bfd6b76b3f01d8217a7c
Rafael <rafael.amoyan@mail.ru>
2022-04-10 14:21:33 +0300

    second less

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,25 @@
 "use strict"
 
-let number = 5;
-const leftBorderWidth = 1;
+let number = 4.6;
+console.log (4.6/0);
+console.log ('string'*6)
+const persone = "Alex";
 
-leftBorderWidth = 10;
-console.log(leftBorderWidth);
+const bool = true;
+
+console.log (something);
+
+let und;
+console.log(und);
+
+
+const obj = {
+    name: "John",
+    age: 25,
+    isMarried: false
+}
+
+console.log(obj.name);
+
+let arr = ['plum.png', 'orange.jpeg', 6 , 'apple.bmp', {}, []]
+console.log(arr[1])
