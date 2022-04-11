9fc2f7afebf139ebb87f7ee76d61e5ce0c9570a2
Rafael <rafael.amoyan@mail.ru>
2022-04-11 12:37:59 +0300

    first lesson

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,10 @@
 "use strict"
 
-// // alert('hello world!');
-// // const result = confirm("Are you here?");
+alert('hello world!');
+const result = confirm("Are you here?");
 
-// const answer = +prompt("Вам есть 18?", "18");
-// console.log (answer + 15);
+const answer = +prompt("Вам есть 18?", "18");
+console.log (answer + 15);
 
 const answers = [];
 
@@ -13,3 +13,16 @@ answers[1] = prompt("Как ваша фамилия? ", "");
 answers[2] = prompt("Сколько вам лет?", "");
 
 console.log(typeof(answers))
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(++incr);
+console.log(--decr);
+
+
+console.log("10"* "2");
+console.log(2*4 == 8);
