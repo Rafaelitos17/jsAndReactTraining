cd92a164510d85441b990b3c4a65a4604ced5470
Rafael <rafael.amoyan@mail.ru>
2022-04-11 13:00:46 +0300

    first lesson

--- src/js/script.js
+++ src/js/script.js
@@ -1,28 +1,25 @@
 "use strict"
 
-alert('hello world!');
-const result = confirm("Are you here?");
-
-const answer = +prompt("Вам есть 18?", "18");
-console.log (answer + 15);
-
-const answers = [];
-
-answers[0] = prompt("Как ваше имя? ", "");
-answers[1] = prompt("Как ваша фамилия? ", "");
-answers[2] = prompt("Сколько вам лет?", "");
-
-console.log(typeof(answers))
-
-let incr = 10,
-    decr = 10;
-
-incr++;
-decr--;
-
-console.log(++incr);
-console.log(--decr);
 
 
-console.log("10"* "2");
-console.log(2*4 == 8);
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+      b = prompt("На сколько оцените его", ""),
+      c = prompt("Один из последних просмотренных фильмов?", ""),
+      d = prompt("На сколько оцените его", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+
+console.log(personalMovieDB);
