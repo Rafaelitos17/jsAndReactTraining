521d6d9761ba43260ff92eaa23d21ff82ddb329d
Rafael <rafael.amoyan@mail.ru>
2022-04-11 13:59:35 +0300

    first lesson

--- src/js/script.js
+++ src/js/script.js
@@ -2,24 +2,45 @@
 
 
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+// const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-
-
-const a = prompt("Один из последних просмотренных фильмов?", ""),
-      b = prompt("На сколько оцените его", ""),
-      c = prompt("Один из последних просмотренных фильмов?", ""),
-      d = prompt("На сколько оцените его", "");
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
 
-console.log(personalMovieDB);
+// const a = prompt("Один из последних просмотренных фильмов?", ""),
+//       b = prompt("На сколько оцените его", ""),
+//       c = prompt("Один из последних просмотренных фильмов?", ""),
+//       d = prompt("На сколько оцените его", "");
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+
+// console.log(personalMovieDB);
+
+
+
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
+// }
+
+// do {
+//     console.log(num);
+//     num++;
+// } 
+// while (num <= 55);
+
+
+for (let i = 1; i < 10; i++) {
+    if (i === 6) {
+        break;
+    }
+    console.log(i);
+}
