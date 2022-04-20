876ef6d783ed5403cbd0d7253d284c32d8b45c53
Rafael <rafael.amoyan@mail.ru>
2022-04-20 18:33:45 +0300

    next commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,23 @@
+// let myArray = [
+//     [1, 2, 3],
+//     [4, 5, 6]
+//     [7, 8, 9],
+//     [[10, 11, 12], 13, 14],
+//   ];
 
+//   const myData = myArray[2][1];
+//   const arr = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9],
+//     [[10, 11, 12], 13, 14]
+//   ];
 
-let myArray = [
-    [1, 2, 3],
-    [4, 5, 6]
-    [7, 8, 9],
-    [[10, 11, 12], 13, 14],
-  ];
-  
-  const myData = myArray[2][1];
-  const arr = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-    [[10, 11, 12], 13, 14]
-  ];
+const myArray = [
+  ["John", 23],
+  ["cat", 2]
+];
+const removedFromMyArray = myArray.pop(0);
+
+console.log(removedFromMyArray);
+console.log(myArray);
