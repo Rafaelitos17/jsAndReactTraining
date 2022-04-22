eaab2a2b70ee5c94b23964e5b3b224f60285f7ec
Rafael <rafael.amoyan@mail.ru>
2022-04-22 22:08:17 +0300

    last commit

--- src/js/script.js
+++ src/js/script.js
@@ -11,13 +11,20 @@
 //     [4, 5, 6],
 //     [7, 8, 9],
 //     [[10, 11, 12], 13, 14]
-//   ];
+// //   ];
 
-const myArray = [
-  ["John", 23],
-  ["cat", 2]
-];
-const removedFromMyArray = myArray.pop(0);
+// const myArray = [
+//   ["John", 23],
+//   ["cat", 2]
+// ];
+// const removedFromMyArray = myArray.pop(0);
 
-console.log(removedFromMyArray);
-console.log(myArray);
+// console.log(removedFromMyArray);
+// console.log(myArray);
+
+
+let i = 30;
+while (i > 0 ) {
+  console.log(i);
+  i--;
+}
