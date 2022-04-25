c846947e378945f0ca161bcbe247ff2f8444a414
Rafael <rafael.amoyan@mail.ru>
2022-04-25 22:32:14 +0300

    itink

--- src/js/script.js
+++ src/js/script.js
@@ -23,8 +23,28 @@
 // console.log(myArray);
 
 
-let i = 30;
-while (i > 0 ) {
-  console.log(i);
-  i--;
-}
+// let i = 30;
+// while (i > 0 ) {
+//   console.log(i);
+//   i--;
+// // }
+// let arr = [1, 2, 3, 4, 5];
+
+// let result = arr.map(function(elem ) {
+// 	console.log(elem); // последовательно выведет 1, 2, 3, 4, 5
+// });
+// console.log(arr);
+
+
+// Дан массив с числами. Используя метод map извлеките из каждого элемента массива квадратный корень и запишите результат в новый массив.
+// let arr = [4, 9, 16, 25];
+// let result = arr.map(function(elem) {
+//   return Math.sqrt(elem);
+// });
+
+// console.log(result);
+// function sum (a, b) {
+//   return a * b;
+// }
+
+// console.log(sum(4, 5));
